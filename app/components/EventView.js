8d10a10434ad7bc0fdb461bdc8082b00686451bd
gincwang <gincwang@gmail.com>
2016-02-04 12:33:09 -0800

    add event details and styling to eventView page

--- app/components/EventView.js
+++ app/components/EventView.js
@@ -1,6 +1,34 @@
 import React from 'react';
 import connectToStores from 'alt-utils/lib/connectToStores';
 import WebAPIStore from '../stores/WebAPIStore';
+import moment from 'moment';
+
+let style = {
+    ul: {
+        padding: '15px 0',
+        backgroundColor: 'lightGray'
+    },
+    li: {
+        listStyle: 'none',
+        margin: '0 20px 20px',
+        display: 'flex',
+        justifyContent: 'flex-start',
+        backgroundColor: 'white'
+    },
+    img: {
+        height: '150px',
+        padding: '10px'
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: '1.2em'
+    },
+    info: {
+        display: 'flex',
+        flexFlow: 'column nowrap',
+        justifyContent: 'space-between'
+    }
+}
 
 class EventView extends React.Component {
     static getStores(){
@@ -9,23 +37,42 @@ class EventView extends React.Component {
     static getPropsFromStores(){
         return WebAPIStore.getState();
     }
+    handleClick(id){
+        console.log(id);
+    }
     render(){
         console.log('eventView: ');
         console.log(this.props.events);
         if(!this.props.events.length){
-            return <div>Your events will be displayed here</div>
+            return <div>Your events will be displayed here</div>;
         }
 
         return (
-            <ul>
-                {this.props.events.map((event) => {
-                    let img = '';
-                    if(event.logo){
-                        img = <img src={event.logo.url} />;
-                    }
-                    return <li key={event.id}>{img}{event.name.html}</li>;
-                })}
-            </ul>
+            <div>
+                <ul style={style.ul}>
+                    {this.props.events.map((event) => {
+                        let img = <img style={style.img} src='app/asset/img/dummyImg.jpg' />;
+                        let venue = '';
+                        if(event.logo){
+                            img = <img style={style.img} src={event.logo.url} />;
+                        }
+                        if(event.venue){
+                            venue = <div><p>{event.venue.name}</p><p>{event.venue.address.city}, {event.venue.address.region}</p></div>;
+                        }
+                        return (<li style={style.li} key={event.id} alt='logo' onClick={this.handleClick.bind(this, event.id)}>
+                                    <div>
+                                        {img}
+                                    </div>
+                                    <div style={style.info}>
+                                        <p>{moment.utc((event.start.utc)).format('ddd, MMM Do - h:mmA')}</p>
+                                        <p style={style.title}>{event.name.html}</p>
+                                        {venue}
+                                    </div>
+                                </li>
+                        );
+                    })}
+                </ul>
+            </div>
         )
 
 
